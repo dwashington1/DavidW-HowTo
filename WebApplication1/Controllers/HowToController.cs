e540663c5f75242db84d4f22b425e723775d62f0
dwashington1 <davidw@WN7-D-DAVIDW2.mtolympus.partners.directs.com>
2015-05-13 18:07:15 -0500

    How To
    Everything on my how to website.

---
+++ WebApplication1/Controllers/HowToController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HowToController : Controller
+    {
+        // GET: HowTo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MakePizza()
+        {
+            return View();
+        }
+        public ActionResult TricepPushup()
+        {
+            return View();
+        }
+        public ActionResult PawnChess()
+        {
+            return View();
+        }
+        public ActionResult Freestyle()
+        {
+            return View();
+        }
+        public ActionResult Lightbulb()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Lightbulb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class Lightbulb
+    {
+        public int id { get; set; }
+        public string TableName { get; set; }
+        public int Step { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class HowTo : DbContext
+    {
+        public DbSet<HowTo> ALightBulb { get; set; }
+    }
+}
